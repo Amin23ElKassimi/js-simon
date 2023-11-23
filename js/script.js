4e9b0d2e660ba9d3caa1cc6f0450980a77cd2b43
Amin23ElKassimi <aminelkassimi01@gmail.com>
2023-11-23 18:07:32 +0100

    Setup-PRJ

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// 23-11-2023
+// Ciao ragazze e ciao ragazzi!
+// Esercizio di oggi: :orologio1: Boom Countdown :collisione:
+// nome repo: js-simon'
+
+
+// Traccia
+// Sfruttiamo le timing functions per fare il conto alla rovescia per la fine di quest'anno!
+// Che scadra' quindi a capodanno!
+
+
+// Consigli
+// Questo esercizio richiede un po' di ricerca ma anche un po' di domande che accompagnano l'uomo da tempo immemore:
+// Da quante ore è composto un giorno?
+// Da quanti minuti è composta un'ora?
+// Da quanti secondi è composto un minuto?
+// Da quanti millisecondi è composto un secondo?
+// Quanti millisecondi mi separano da domani alle 9:30?
+// Esiste un oggetto JS in grado di gestire le date?
+// Esistono dei metodi per trasformare una data in millisecondi?
+
+
+
